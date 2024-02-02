f1b3d855d657ad148a85c538b1e806da225cbddb
Rbrahma1245 <rbrahma1245@gmail.com>
2024-02-02 09:22:51 +0530

    added image draggable

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -3,6 +3,8 @@ import { Component } from "react";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import "@react-pdf-viewer/core/lib/styles/index.css";
+import Draggable from "react-draggable";
+import { Resizable } from "re-resizable";
 
 class Display extends Component {
   constructor() {
@@ -21,22 +23,27 @@ class Display extends Component {
                 <Viewer fileUrl={this.props.pdfFile} />
 
                 {/* Image */}
-                {this.props.image?.url == null ? (
-                  null
-                ) : (
-                  <img
-                    src={this.props.image?.url}
-                    style={{
-                      position: "absolute",
-                      zIndex: 1,
-                      top: 12,
-                      left: "50%",
-                      width: 200,
-                      height: 150,
-                      objectFit: "contain",
-                      border:"1px solid black"
-                    }}
-                  />
+                {this.props.image?.url == null ? null : (
+                  <Draggable>
+                    <Resizable
+                      defaultSize={{
+                        width: 200,
+                        height: 150,
+                      }}
+                      style={{
+                        background: `url(${this.props.image.url})`,
+                        backgroundSize: "contain",
+                        backgroundRepeat: "no-repeat",
+                        position: "absolute",
+                        zIndex: 1,
+                        top: 12,
+                        left: "50%",
+                        objectFit: "contain",
+                        border: "1px solid black",
+                      }}
+                      lockAspectRatio={true}
+                    ></Resizable>
+                  </Draggable>
                 )}
               </div>
             </>
